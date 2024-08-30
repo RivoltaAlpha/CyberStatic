d75b4a0e0cc2dadd36ef3fdb01b1d18adeefdee6
RivoltaAlpha <mwanikitiffany25@gmail.com>
2024-08-30 13:59:59 +0300

    bg and styling

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (
-        <>
-            <div className="mx-auto max-w-7xl px-4 text-neutral-content-200 bg-base-300">
+        <div className="bg-gray-900 text-white">
+            <div className="mx-auto max-w-7xl px-4 text-neutral-content-200">
                 {/* Hero Map */}
                 <div className="flex flex-col space-y-8 pb-10 pt-12 md:pt-24">
                     <p className="text-center text-3xl font-bold content md:text-5xl md:leading-10">
@@ -19,20 +19,20 @@ export default function Contact() {
                         <div className="flex items-center justify-center">
                             <div className="px-2 md:px-12">
                                 <p className="text-2xl font-bold text-base-content-100 md:text-4xl">Get in touch</p>
-                                <p className="mt-4 text-lg text-gray-600">
+                                <p className="mt-4 text-lg ">
                                     Our friendly team would love to hear from you.
                                 </p>
                                 <form action="" className="mt-8 space-y-4">
                                     <div className="grid w-full gap-y-4 md:gap-x-4 lg:grid-cols-2">
                                         <div className="grid w-full  items-center gap-1.5">
                                             <label
-                                                className="text-sm font-medium leading-none text-black peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                                                 htmlFor="first_name"
                                             >
                                                 First Name
                                             </label>
                                             <input
-                                                className="flex h-10 w-full rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-black dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                                                className="flex h-10 w-full rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                                                 type="text"
                                                 id="first_name"
                                                 placeholder="First Name"
@@ -40,13 +40,13 @@ export default function Contact() {
                                         </div>
                                         <div className="grid w-full  items-center gap-1.5">
                                             <label
-                                                className="text-sm font-medium leading-none text-black peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                                className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                                                 htmlFor="last_name"
                                             >
                                                 Last Name
                                             </label>
                                             <input
-                                                className="flex h-10 w-full rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-black dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                                                className="flex h-10 w-full rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                                                 type="text"
                                                 id="last_name"
                                                 placeholder="Last Name"
@@ -55,13 +55,13 @@ export default function Contact() {
                                     </div>
                                     <div className="grid w-full  items-center gap-1.5">
                                         <label
-                                            className="text-sm font-medium leading-none text-black peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                            className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                                             htmlFor="email"
                                         >
                                             Email
                                         </label>
                                         <input
-                                            className="flex h-10 w-full rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-black dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                                            className="flex h-10 w-full rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                                             type="text"
                                             id="email"
                                             placeholder="Email"
@@ -69,13 +69,13 @@ export default function Contact() {
                                     </div>
                                     <div className="grid w-full  items-center gap-1.5">
                                         <label
-                                            className="text-sm font-medium leading-none text-black peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                            className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                                             htmlFor="phone_number"
                                         >
                                             Phone number
                                         </label>
                                         <input
-                                            className="flex h-10 w-full rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-black dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                                            className="flex h-10 w-full rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-gray-400 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white dark:text-gray-50 dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                                             type="tel"
                                             id="phone_number"
                                             placeholder="Phone number"
@@ -83,13 +83,13 @@ export default function Contact() {
                                     </div>
                                     <div className="grid w-full  items-center gap-1.5">
                                         <label
-                                            className="text-sm font-medium leading-none text-black peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                                            className="text-sm font-medium leading-none text-white peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                                             htmlFor="message"
                                         >
                                             Message
                                         </label>
                                         <textarea
-                                            className="flex h-10 w-full rounded-md border border-black bg-transparent px-3 py-2 text-sm placeholder:text-black focus:outline-none focus:ring-1 focus:ring-black focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-black dark:text-black dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
+                                            className="flex h-10 w-full rounded-md border border-white bg-transparent px-3 py-2 text-sm placeholder:text-white focus:outline-none focus:ring-1 focus:ring-white focus:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50 dark:border-white dark:text-white dark:focus:ring-gray-400 dark:focus:ring-offset-gray-900"
                                             id="message"
                                             placeholder="Leave us a message"
                                             cols={3}
@@ -107,6 +107,6 @@ export default function Contact() {
                 </div>
             </div>
             <hr className="mt-6" />
-        </>
+        </div>
     );
 }  
